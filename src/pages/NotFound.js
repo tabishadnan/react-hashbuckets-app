3a40bc52b03d7dcbbff657cafc9887b82adb713c
tabishadnan <tabishadnan000@gmail.com>
2021-02-17 11:05:06 +0500

    fix hosting

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return ( 
         <div className="not-found">
             <h3>404 Not Found</h3>
+            <p><Link to="/">Go To Home</Link></p>
         </div>
      );
 }
